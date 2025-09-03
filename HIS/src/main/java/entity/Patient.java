731e75cc58a92a632bfa4775146e17c499257d4f
peiju <peiju.liaooo@gmail.com>
2025-09-03 18:33:31 +0800

    建立實體類別與資料庫的ORM

--- HIS/src/main/java/entity/Patient.java
+++ HIS/src/main/java/entity/Patient.java
@@ -1,4 +1,124 @@
 package entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="patients")
 public class Patient {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="first_name")
+    private String firstName; // 名
+
+    @Column(name="last_name")
+    private String lastName; // 姓
+
+    @Column(name="email")
+    private String email; // 信箱
+
+    @Column(name="id_number")
+    private String idNumber; // 身分證字號
+
+    @Column(name="birth_date")
+    private String birthDate; // 生日
+
+    @Column(name="gender")
+    private String gender; // 性別
+
+    @Column(name="created_at")
+    private String createdAt; // 建立時間
+    public Patient() {}
+
+    public Patient(int id, String firstName, String lastName, String email, String idNumber, String birthDate, String gender, String createdAt) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.idNumber = idNumber;
+        this.birthDate = birthDate;
+        this.gender = gender;
+        this.createdAt = createdAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getIdNumber() {
+        return idNumber;
+    }
+
+    public void setIdNumber(String idNumber) {
+        this.idNumber = idNumber;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", idNumber='" + idNumber + '\'' +
+                ", birthDate='" + birthDate + '\'' +
+                ", gender='" + gender + '\'' +
+                ", createdAt='" + createdAt + '\'' +
+                '}';
+    }
 }
