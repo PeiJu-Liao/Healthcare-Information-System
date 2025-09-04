8c51d9984d668b0e6b63f2eed427cbbc17b7a941
peiju <peiju.liaooo@gmail.com>
2025-09-04 17:44:38 +0800

    Entity-Patient field update

--- HIS/src/main/java/entity/Patient.java
+++ HIS/src/main/java/entity/Patient.java
@@ -8,48 +8,44 @@ public class Patient {
 
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
-    @Column(name="id")
-    private int id;
+    @Column(name="patient_id")
+    private int patientId; // PK
 
-    @Column(name="first_name")
+    private String nationalId; // 身分證字號
     private String firstName; // 名
-
-    @Column(name="last_name")
     private String lastName; // 姓
-
-    @Column(name="email")
     private String email; // 信箱
-
-    @Column(name="id_number")
-    private String idNumber; // 身分證字號
-
-    @Column(name="birth_date")
     private String birthDate; // 生日
-
-    @Column(name="gender")
     private String gender; // 性別
-
-    @Column(name="created_at")
     private String createdAt; // 建立時間
+
     public Patient() {}
 
-    public Patient(int id, String firstName, String lastName, String email, String idNumber, String birthDate, String gender, String createdAt) {
-        this.id = id;
+    public Patient(int patientId, String nationalId, String firstName, String lastName, String email, String birthDate, String gender, String createdAt) {
+        this.patientId = patientId;
+        this.nationalId = nationalId;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
-        this.idNumber = idNumber;
         this.birthDate = birthDate;
         this.gender = gender;
         this.createdAt = createdAt;
     }
 
     public int getId() {
-        return id;
+        return patientId;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setId(int patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getNationalId() {
+        return nationalId;
+    }
+
+    public void setNationalId(String nationalId) {
+        this.nationalId = nationalId;
     }
 
     public String getFirstName() {
@@ -74,14 +70,6 @@ public class Patient {
 
     public void setEmail(String email) {
         this.email = email;
-    }
-
-    public String getIdNumber() {
-        return idNumber;
-    }
-
-    public void setIdNumber(String idNumber) {
-        this.idNumber = idNumber;
     }
 
     public String getBirthDate() {
@@ -111,11 +99,11 @@ public class Patient {
     @Override
     public String toString() {
         return "Patient{" +
-                "id=" + id +
+                "patientId=" + patientId +
+                ", nationalId='" + nationalId + '\'' +
                 ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
                 ", email='" + email + '\'' +
-                ", idNumber='" + idNumber + '\'' +
                 ", birthDate='" + birthDate + '\'' +
                 ", gender='" + gender + '\'' +
                 ", createdAt='" + createdAt + '\'' +
